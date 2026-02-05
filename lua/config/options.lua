34ac8518882dd72f6c93b0af5b2d3f809e6c23cf
voidslvt <contact@voidslvt.ru>
2026-02-05 22:57:52 +0300

    Edited options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,38 +1,14 @@
 -- Общее
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamed,unnamedplus'
 vim.opt.laststatus = 3
+vim.opt.undofile = true
 
 -- Табы
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
-
-local function set_indent(size)
-  vim.opt_local.expandtab = true
-  vim.opt_local.shiftwidth = size
-  vim.opt_local.tabstop = size
-end
-
--- Группируем автокоманды, чтобы они не дублировались при перезагрузке конфига
-local indent_group = vim.api.nvim_create_augroup("IndentSettings", { clear = true })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "python" },
-  group = indent_group,
-  callback = function()
-    set_indent(4)
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "lua" },
-  group = indent_group,
-  callback = function()
-    set_indent(2)
-  end,
-})
+vim.opt.softtabstop = 4
+vim.opt.smartindent = true
 
 -- Поиск
 vim.opt.ignorecase = true
@@ -41,13 +17,14 @@ vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- UI
+vim.opt.number = true
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 vim.opt.signcolumn = 'yes'
 vim.opt.wrap = false
-vim.opt.scrolloff = 8
-vim.opt.showmode = false
-vim.o.winborder = 'rounded'
+vim.opt.winborder = 'rounded'
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
 -- Подсветка скопированного текста
 vim.api.nvim_create_autocmd("TextYankPost", {
